527565df9b5e30b92a757e492121790b33c624b7
Alexander Chen <alexander.qili.chen@gmail.com>
2023-12-27 14:01:49 +0800

    Added patch /orders/:orderId endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import bodyParser from "body-parser";
+
 import { router } from "./routes";
 
 const app = express();
+app.use(bodyParser.json());
+
 app.use("/", router);
 
 app.listen(process.env.PORT, () =>
--- backend/src/routes/orders.ts
+++ backend/src/routes/orders.ts
@@ -7,22 +7,22 @@ import { getPaginationOptions } from "../helpers/pagination";
 export const router = express.Router();
 const prisma = new PrismaClient();
 
-const querySchema = Joi.object({
-  limit: Joi.number().integer().min(1),
-  offset: Joi.number().integer().min(0),
-  status: Joi.string()
-    .uppercase()
-    .valid(...Object.keys(OrderStatus)),
-});
-
 router.get("/", async (req, res) => {
-  const { error, value } = querySchema.validate(req.query);
+  const querySchema = Joi.object({
+    limit: Joi.number().integer().min(1),
+    offset: Joi.number().integer().min(0),
+    status: Joi.string()
+      .uppercase()
+      .valid(...Object.keys(OrderStatus)),
+  });
 
-  if (error) {
-    return res.status(400).json({ error });
+  const { error: queryError, value: query } = querySchema.validate(req.query);
+
+  if (queryError) {
+    return res.status(400).json({ error: { queryError } });
   }
 
-  const { limit, offset, status } = value;
+  const { limit, offset, status } = query;
 
   const prismaOptions = {
     skip: offset,
@@ -34,5 +34,39 @@ router.get("/", async (req, res) => {
 
   const orders = await prisma.order.findMany(prismaOptions);
 
-  return res.json(orders);
+  return res.status(200).json(orders);
+});
+
+router.patch("/:orderId", async (req, res) => {
+  const paramsSchema = Joi.object({
+    orderId: Joi.number().integer().min(1),
+  });
+  const bodySchema = Joi.object({
+    status: Joi.string()
+      .uppercase()
+      .valid(...Object.keys(OrderStatus)),
+  });
+
+  const { error: paramsError, value: params } = paramsSchema.validate(
+    req.params
+  );
+  const { error: bodyError, value: body } = bodySchema.validate(req.body);
+
+  if (paramsError || bodyError) {
+    return res.status(400).json({ error: { paramsError, bodyError } });
+  }
+
+  const { orderId } = params;
+  const { status } = body;
+
+  const order = await prisma.order.update({
+    where: {
+      id: orderId,
+    },
+    data: {
+      status,
+    },
+  });
+
+  return res.status(200).json(order);
 });
