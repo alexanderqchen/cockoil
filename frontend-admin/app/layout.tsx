63052ce1ff46aa5cbe50599508588a698ecf19b1
Alexander Chen <alexander.qili.chen@gmail.com>
2024-02-09 09:09:21 +0800

    Update title to admin site

--- frontend-admin/app/layout.tsx
+++ frontend-admin/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CockOil Affiliate Site",
+  title: "CockOil Admin Site",
   description: "Earn cash by referring others.",
 };
 
